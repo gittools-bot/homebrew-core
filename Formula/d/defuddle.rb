c4106694659ccca61c015781cd2a1ffcb56b4b73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 20:44:38 +0000

    defuddle: update 0.19.2 bottle.

--- Formula/d/defuddle.rb
+++ Formula/d/defuddle.rb
@@ -6,7 +6,7 @@ class Defuddle < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4956bc8858812e1060e9cf579aa6a03e24fd46ae4cd73099086231db57a71b24"
+    sha256 cellar: :any_skip_relocation, all: "068ff99fa5e9c100c4880bbf80e9b64c6d25f842e631477a4a17882c4466c94f"
   end
 
   depends_on "node"
