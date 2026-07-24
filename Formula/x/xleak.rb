a28e95f2501ee030a25b633043b09f9836f5629f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-24 11:19:41 +0000

    xleak: update 0.2.6 bottle.

--- Formula/x/xleak.rb
+++ Formula/x/xleak.rb
@@ -7,12 +7,13 @@ class Xleak < Formula
   head "https://github.com/bgreenwell/xleak.git", branch: "devel"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8a4345f10aa442799b997922a5b20cfca50fb113225bf664c71433085bf43009"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a70a3933cb3c4dcd72610501c8a6daf30d712af1301ecbf7f4cff35420e51d39"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "59bdb6e305d06f1dcce2b529e971187cb6b2a66db4ba6eca65d8a31ad46fd6c7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "a9cd2ad6c3e8c3e48077c112bba3e0d8f53a7c3aa366e0e7adcbd05c4c14a706"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "af30ca4efbc3d65597495183954fb7f4224c47f3a2bcc1cacb46a2d1fd8c4c21"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "240c8ad7c7117ddfe275fab219521d6e7f63cab5d4d24c7522e620cbee6de25e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "14ade8276fec7fa62c7da39e7807e536862bb932fcb24de73242d34b56e88c1a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0b4ed79469e9eedc6aeac6a814098ff6bd105cc6ffa8d07b4c743653b068b8c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b1acb59afb191b7f8a405bc5c06d9fd9ce38acf95d5656ab2592eafe5724649e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "781a1471106577c6e2ac4e6125e50b0eb1ab6cc7138b0552108120683d2c5f7e"
+    sha256 cellar: :any,                 arm64_linux:   "bcdde47d70673544d06122740138000e125fdef2ebcd4ab3e803333d2818e88d"
+    sha256 cellar: :any,                 x86_64_linux:  "5fdb88fde4eeef24e605a317bbde5008e89a02967962ea0e4263063b9d9e7f71"
   end
 
   depends_on "rust" => :build
