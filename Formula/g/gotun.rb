ffaf161cc77d56bb05db6d81e0106191940ccb0e
Rui Chen <rui@chenrui.dev>
2026-09-20 12:08:22 -0400

    gotun: use allow_network_access! :test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gotun.rb
+++ Formula/g/gotun.rb
@@ -17,6 +17,12 @@ class Gotun < Formula
 
   depends_on "go" => :build
 
+  allow_network_access! :test
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.Version=#{version}"), "./cmd/gotun"
     generate_completions_from_executable(bin/"gotun", shell_parameter_format: :cobra)
