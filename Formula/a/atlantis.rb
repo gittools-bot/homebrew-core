8393fab92f1517b8bc3334b33d683b7f3202c3d8
Rui Chen <rui@chenrui.dev>
2026-09-20 12:04:20 -0400

    atlantis: use allow_network_access! :test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/atlantis.rb
+++ Formula/a/atlantis.rb
@@ -22,6 +22,12 @@ class Atlantis < Formula
   depends_on "go" => :build
   depends_on "opentofu" => :test
 
+  allow_network_access! :test
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     # The commit variable only displays 7 characters, so we can't use #{tap.user} or "Homebrew".
     ldflags = %W[
