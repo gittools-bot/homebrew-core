90f1a34e375e711e4aa6f08414279b8b2cf6d8e3
Rui Chen <rui@chenrui.dev>
2026-09-23 03:15:35 -0400

    avro-c: allow test network
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/avro-c.rb
+++ Formula/a/avro-c.rb
@@ -26,6 +26,8 @@ class AvroC < Formula
     depends_on "zlib-ng-compat"
   end
 
+  allow_network_access! :test
+
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
