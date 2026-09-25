0fa24853d27de412d31438a38ac923e75ff07ff5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 20:57:17 +0000

    typescript-language-server 6.0.1

--- Formula/t/typescript-language-server.rb
+++ Formula/t/typescript-language-server.rb
@@ -1,8 +1,8 @@
 class TypescriptLanguageServer < Formula
   desc "Language Server Protocol implementation for TypeScript wrapping tsserver"
   homepage "https://github.com/typescript-language-server/typescript-language-server"
-  url "https://registry.npmjs.org/typescript-language-server/-/typescript-language-server-6.0.0.tgz"
-  sha256 "6e23b48efc76af4e70928cdfe62ea6e6cfef67ab4c1e7579c4e82dd284fbdfd2"
+  url "https://registry.npmjs.org/typescript-language-server/-/typescript-language-server-6.0.1.tgz"
+  sha256 "85eabb9251d85d3798b247b2bc0895ca111d866bfbf59533f64b2461b0d8649f"
   license all_of: ["MIT", "Apache-2.0"]
 
   bottle do
