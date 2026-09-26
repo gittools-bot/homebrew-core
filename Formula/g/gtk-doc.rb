a459c58bbd232119ac0a2b53ef9c37a0a04bda2d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 21:10:00 +0000

    gtk-doc: update 1.37.0 bottle.

--- Formula/g/gtk-doc.rb
+++ Formula/g/gtk-doc.rb
@@ -15,13 +15,11 @@ class GtkDoc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "148169825f695d4e693f2efd71530d16ecf08bbd7641a1e613a4b75d3703e1dd"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "10ffbf3cbe6b5a3aa9707a9ac01c256b208a92f179fa279ba1c42aa641bc36c7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "faf1207cdda1baeb9fe4b6282394afc9465ac6192d356315ec3f590a5f354896"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:      "0e1d337546ff4c8dda08e1d70069985675009c2c88e1c510c5b278e6bf62ee29"
-    sha256 cellar: :any_skip_relocation, sonoma:            "4899cf390c65bef2d73d072f1a228bc2def11be1480e4a2f74f23609bf439325"
-    sha256 cellar: :any_skip_relocation, arm64_linux:       "cf876fe048491a3d10bd980e4022ac7cce4761a931651b29d85606a0bb06fc33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:      "7dfea56e91394088e0dc7b8fc79a56810342b8981c86a3b45eec0baca3aa6b2a"
+    sha256 cellar: :any_skip_relocation, arm64_golden_gate: "7d1898204cd8ca31f070f6330031edd3f1dfa73dc9deba726bc70820bba45f25"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:       "c60299b698203012eca8ba77eb2ef628e2e9a49c76b3fac7f649219e090723c5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:     "cb13c51e5ef584c0088a2a9696ca55ecb304bb281503aaed354c4be0f8321080"
+    sha256 cellar: :any,                 arm64_linux:       "8ede875de3135522b871f54fe7898a712d39af955a3bf42db0a8aa9e68b0786a"
+    sha256 cellar: :any,                 x86_64_linux:      "85fcd7f3589814d422c46975c53a1b1e187b8187e07a4a2f693b4075555cad88"
   end
 
   depends_on "meson" => :build
